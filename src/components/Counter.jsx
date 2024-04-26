b8f9fcdcb05f9cbebc1358082bf2004e3975c5d9
parul297 <parulsharma3402@gmail.com>
2024-04-26 16:22:55 +0530

    changes made again

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 
 const  Counter = ()=>{
-  const [Count , UpdateCount] = useState(0);
+  const [count , setCount] = useState(0);
   const handleIncrement = () =>{
-    UpdateCount((prevState)=>prevState+1)
+   setCount((prevState)=>prevState+1)
   }
   const handleDecrement = () =>{
-    UpdateCount((prevState)=>prevState-1)
+    setCount((prevState)=>prevState-1)
   }
     return (
     <div>
         <h1>Counter App</h1>
-        <p>Count:{Count}</p>
+        <p>Count: {count}</p>
         <button onClick={handleIncrement}>Increment</button>
         <button onClick={handleDecrement}>Decrement</button>
 
